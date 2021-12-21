956eaf85be7412a5b69a40b6e88f72d162bd77c8
KevinFernandez16 <kjfernandez1456@gmail.com>
2021-12-20 21:07:15 -0500

    Added add campus button

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -1,75 +1,13 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  title: {
-    flexGrow: 1,
-    textAlign: 'left',
-    fontType: 'bold',
-    fontFamily: 'Courier, sans-serif', 
-    fontSize: '35px', 
-    color: '#EAE0D5'
-  },
-  appBar:{
-    backgroundColor: '#087E8B',
-    shadows: ['none'],
-  },
-  greeting:{
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-    width: "50%",
-    margin: "auto",
-  },
-  links:{
-    textDecoration: 'none',
-  }
-
-}));
-
 const AllCampusesView = (props) => {
-  const classes = useStyles();
   if (!props.allCampuses.length) {
     return <div>There are no campuses.</div>;
   }
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static" elevation={0} className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" className={classes.title} color="inherit" >
-            CRUD App
-          </Typography>
-
-          <Link className={classes.links} to={'/'} >
-            <Button variant="contained" color="#979DAC" style={{marginRight: '10px'}}>
-              HomePage
-            </Button>
-          </Link>
-
-          <Link className={classes.links} to={'/campuses'} >
-            <Button variant="contained" color="#979DAC" style={{marginRight: '10px'}}>
-              All Campuses
-            </Button>
-          </Link>
-
-          <Link className={classes.links} to={'/students'} >
-            <Button variant="contained" color="#979DAC">
-              All Students
-            </Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-      
+    <div>
       {props.allCampuses.map((campus) => (
         <div key={campus.id}>
           <Link to={`/campus/${campus.id}`}>
@@ -78,6 +16,9 @@ const AllCampusesView = (props) => {
           <p>{campus.description}</p>
         </div>
       ))}
+      <Link to={`/newCampus`}>
+        <button>Add Campus</button>
+      </Link>
     </div>
   );
 };
